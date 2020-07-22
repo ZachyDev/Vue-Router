9690c5e58b369499b19d0ab6cdd2bcd0a09222b2
ZachyDev <mosetizachary001@gmail.com>
2020-07-23 01:44:45 +0300

    created my own custom routes

--- travel_smart/src/store.js
+++ travel_smart/src/store.js
@@ -115,9 +115,9 @@ export default {
       ]
     },
     {
-      name: "Jamaica",
-      slug: "jamaica",
-      image: "jamaica.jpg",
+      name: "Kenya",
+      slug: "kenya",
+      image: "kenya.jpg",
       id: 4,
       description:
         "all about jamaica. Suspendisse lobortis pharetra tempor. Cras eleifend ante sed arcu interdum, in bibendum enim ultricies. Integer rutrum quis risus at tempor. Maecenas facilisis, nisi vel pellentesque maximus, lectus felis malesuada purus, a pulvinar elit est quis turpis. Duis convallis purus quis finibus consequat. Pellentesque faucibus tincidunt augue non consequat. Donec fringilla at est sit amet blandit. Nunc at porttitor ligula. Fusce sed odio turpis.",
--- travel_smart/src/views/Home.vue
+++ travel_smart/src/views/Home.vue
@@ -1,18 +1,48 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <img alt="Travel Smart" src="../assets/logo.jpg" />
+    <h1>All Destinations</h1>
+    <div class="destinations">
+      <div :key="destination.name" v-for="destination in destinations">
+        <router-link :to="destination.slug"> 
+          {{ destination.name }}
+        </router-link>
+        <figure>
+          <router-link :to="destination.slug">
+            <img :src="require(`@/assets/${ destination.image }`)" :alt="destination.name">
+          </router-link>
+        </figure>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import store from '@/store.js';
 export default {
   name: "Home",
   components: {
-    HelloWorld
+
+  },
+  data() {
+    return {
+      destinations: store.destinations
+    };
   }
 };
 </script>
+<style>
+.home{
+  max-width: 1400px;
+  margin: 0 auto;
+}
+img{
+  max-width:200px;
+}
+.destinations {
+  display: flex;
+  justify-content: space-between;
+}
+
+</style>
