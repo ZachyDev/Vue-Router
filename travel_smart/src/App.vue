ade69940cb40165a49aa6531bb4b829ad1657dbe
ZachyDev <mosetizachary001@gmail.com>
2020-07-23 00:35:57 +0300

    started travel_smart project using vue.js

--- travel_smart/src/App.vue
+++ travel_smart/src/App.vue
@@ -2,7 +2,6 @@
   <div id="app">
     <div id="nav">
      <router-link to="/">Home</router-link> |
-     <a href="/">a -tag </a> |
      <router-link to="/about">About</router-link>
     </div>
     <router-view />
