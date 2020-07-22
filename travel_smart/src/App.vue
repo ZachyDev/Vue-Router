7bfffa8d2e564330ce752c3a386921d836042ce4
ZachyDev <mosetizachary001@gmail.com>
2020-07-23 02:03:24 +0300

    lazy loading and magic comments

--- travel_smart/src/App.vue
+++ travel_smart/src/App.vue
@@ -2,7 +2,6 @@
   <div id="app">
     <div id="nav">
      <router-link to="/">Home</router-link> |
-     <router-link to="/about">About</router-link> |
      <router-link to="/brazil">Brazil</router-link> |
      <router-link to="/hawaii">Hawaii</router-link> |
      <router-link to="/kenya">Kenya</router-link> |
--- travel_smart/src/router/index.js
+++ travel_smart/src/router/index.js
@@ -1,10 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
-import Brazil from '../views/Brazil.vue';
-import Kenya from '../views/Kenya.vue';
-import Hawaii from '../views/Hawaii.vue';
-import Panama from '../views/Panama.vue'
 
 Vue.use(VueRouter);
 
@@ -15,33 +11,28 @@ const routes = [
     component: Home
   },
   {
-    path: "/about",
-    name: "About",
+    path: "/brazil",
+    name: "brazil",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  },
-  {
-    path: '/brazil',
-    name: 'brazil',
-    component: Brazil
+      import(/* webpackChunkName: "brazil" */ '../views/Brazil')
   },
   {
     path: '/kenya',
     name: 'kenya',
-    component: Kenya
+    component: () => import(/* webpackChunkName: "kenya" */ '../views/Kenya')
   },
   {
     path: '/hawaii',
     name: 'hawaii',
-    component: Hawaii
+    component: () => import(/* webpackChunkName: "hawaii" */ '../views/Hawaii')
   },
   {
     path: '/panama',
     name: 'panama',
-    component: Panama
+    component: () => import(/* webpackChunkName: "panama" */ '../views/Panama')
   },
 ];
 
